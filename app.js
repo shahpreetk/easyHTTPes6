d94d6fa14b60b9930a62990df6301325dc2da9d5
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-16 12:34:18 +0530

    PUT Request

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const data={
 }
 
 // CREATE POST
-http.post('https://jsonplaceholder.typicode.com/users',data)
+// http.post('https://jsonplaceholder.typicode.com/users',data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// UPDATE POST 
+http.put('https://jsonplaceholder.typicode.com/users/2',data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
--- easyhttpes6.js
+++ easyhttpes6.js
@@ -10,15 +10,30 @@ class EasyHTTP {
     }
 
     // MAKE AN HTTP POST REQUEST
-    post(url,data) {
+    post(url, data) {
         return new Promise((resolve, reject) => {
             fetch(url, {
-                method:'POST',
-                headers:{
-                    'content-type':'application/json'
-                },
-                body:JSON.stringify(data)
-            })
+                    method: 'POST',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
+    // MAKE AN HTTP PUT REQUEST
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
                 .then(res => res.json())
                 .then(data => resolve(data))
                 .catch(err => reject(err));
