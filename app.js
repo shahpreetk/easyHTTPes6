d85e70a4f4d570197c6953d7171a65efa72762e8
Preet Shah <preetshah@Preets-MacBook-Pro.local>
2019-08-16 12:30:14 +0530

    POST Request

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const http = new EasyHTTP;
 
 // GET USERS
-http.get('https://jsonplaceholder.typicode.com/users')
+// http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// USER DATA
+const data={
+    name:'John Doe',
+    username:'johndoe',
+    email:'jdoe@gmail.com'
+}
+
+// CREATE POST
+http.post('https://jsonplaceholder.typicode.com/users',data)
     .then(data => console.log(data))
     .catch(err => console.log(err));
--- easyhttpes6.js
+++ easyhttpes6.js
@@ -8,4 +8,20 @@ class EasyHTTP {
                 .catch(err => reject(err));
         });
     }
+
+    // MAKE AN HTTP POST REQUEST
+    post(url,data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method:'POST',
+                headers:{
+                    'content-type':'application/json'
+                },
+                body:JSON.stringify(data)
+            })
+                .then(res => res.json())
+                .then(data => resolve(data))
+                .catch(err => reject(err));
+        });
+    }
 }
